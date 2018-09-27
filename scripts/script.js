ef2a2b0100a11f1484b488f2e0377b07e38ec70f
Colon <EC86638@ad1.prod>
2018-09-27 11:28:25 -0400

    basic js completed for one page layout

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+$(document).ready(() => {
+  $('.home-link').click(() => {
+    // $('.home').css('display', 'block');
+    // $('.about').css('display', 'none');
+    // $('.contact').css('display', 'none');
+    // $('.projects').css('display', 'none');
+  });
+
+  $('.about-link').click(() => {
+    // $('.about').css('display', 'block');
+    // $('.home').css('display', 'none');
+    // $('.contact').css('display', 'none');
+    // $('.projects').css('display', 'none');
+  });
+
+  $('.contact-link').click(() => {
+    // $('.contact').css('display', 'block');
+    // $('.about').css('display', 'none');
+    // $('.home').css('display', 'none');
+    // $('.projects').css('display', 'none');
+  });
+
+  $('.projects-link').click(() => {
+    // $('.projects').css('display', 'block');
+    // $('.about').css('display', 'none');
+    // $('.contact').css('display', 'none');
+    // $('.home').css('display', 'none');
+  });
+});
