6fe468bd6ac835ae873dcfe738c515398c476cd0
Emily Colon <38858811+emilycolon@users.noreply.github.com>
2018-10-01 10:23:29 -0400

    adjusted how bkgrd styles applied, added js logic to chg bkgrd on click

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,33 @@
+const getCurrentBackground = () => {
+  let backgroundStyle = $('.background').css('background');
+  let splitBackgroundArray = backgroundStyle.split(') ');
+  let backgroundUrl = splitBackgroundArray[1];
+  let splitUrl = backgroundUrl.split('/');
+  let currentPicture = splitUrl[splitUrl.length - 1];
+  let currentPicArray = currentPicture.split('');
+  let currentPicNumber = currentPicArray[7];
+  changeBackground(currentPicNumber);
+};
+
+const changeBackground = currentPicNumber => {
+  if (currentPicNumber < 9) {
+    currentPicNumber = parseInt(currentPicNumber) + 1;
+    currentPicNumber = currentPicNumber.toString();
+  } else {
+    currentPicNumber = '1';
+  }
+
+  $('.background').css(
+    'background',
+    `url("./images/mandala${currentPicNumber}.jpg") no-repeat center center`
+  );
+};
+
 $(document).ready(() => {
+  $(window).click(() => {
+    getCurrentBackground();
+  });
+
   $('a').click(e => {
     if (e.target.className === 'link') {
       let section = e.target.id;
